4f9c7cb18c0947d839b9b86ab4b557861127ae15
Valentin Richard <valentin.richard@infomaniak.com>
2025-12-07 19:36:49 +0100

    chore: v0.3.4

--- src/bidirectional/well-known-functions/map/bidirectional-map-functions.ts
+++
@@ -1,6 +0,0 @@
-import { type MapFunction } from '../../../well-known-functions/map/map-function.js';
-
-export interface BidirectionalMapFunctions<GIn, GOut> {
-  readonly inOut: MapFunction<GIn, GOut>;
-  readonly outIn: MapFunction<GOut, GIn>;
-}
